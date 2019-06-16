b4a5b5359f1a89fe9c3fff95e8506fc6a9996c8b
jo-es <escherich.johannes+github@gmail.com>
2019-06-16 17:04:03 +0200

    Fix pending schedule calculation in getNextState method

--- contracts/Engines/PAMEngine.sol
+++ contracts/Engines/PAMEngine.sol
@@ -59,7 +59,7 @@ contract PAMEngine is Core, IEngine {
 
 		ProtoEvent[MAX_EVENT_SCHEDULE_SIZE] memory pendingProtoEventSchedule = computeProtoEventScheduleSegment(
 			contractTerms,
-			contractState.lastEventTime,
+			shiftEventTime(contractState.lastEventTime, contractTerms.businessDayConvention, contractTerms.calendar),
 			timestamp
 		);
 
