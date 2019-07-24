f9d66e071ae2da13462538ec0d208ab1739f445f
jo-es <45110941+jo-es@users.noreply.github.com>
2019-07-24 18:26:32 +0200

    Merge feature/shift-led-fix (#9)
    * Fix pending schedule calculation in getNextState method
    
    * Add shift to computeNextMethod in ANNEngine
    
    * Add additional test cases for PAMEngine and ANNEngine
    
    * Refactor tests

--- test/Engines/ANN/TestANNEngine.js
+++ test/Engines/ANN/TestANNEngine.js
@@ -1,6 +1,6 @@
 const ANNEngine = artifacts.require('ANNEngine.sol');
 
-const { getTestCases } = require('../../helper/tests');
+const { getDefaultTestTerms } = require('../../helper/tests');
 const { removeNullEvents } = require('../../helper/schedule');
 
 
@@ -8,12 +8,58 @@ contract('ANNEngine', () => {
 
   before(async () => {        
     this.ANNEngineInstance = await ANNEngine.new();
-    this.terms = (await getTestCases('ANN'))['20001'].terms;
+    this.terms = await getDefaultTestTerms('ANN');
   });
 
   it('should yield the initial contract state', async () => {
     const initialState = await this.ANNEngineInstance.computeInitialState(this.terms, {});
     assert.isTrue(Number(initialState['lastEventTime']) === Number(this.terms['statusDate']));
+  });
+
+  it('should yield the next next contract state and the contract events', async() => {
+    const initialState = await this.ANNEngineInstance.computeInitialState(this.terms, {});
+    await this.ANNEngineInstance.computeNextState(this.terms, initialState, this.terms['maturityDate']);
+  });
+
+  it('should yield the same evaluated events for computeNextState and computeNextStateForProtoEvent', async () => {
+    const initialState = await this.ANNEngineInstance.computeInitialState(this.terms, {});
+
+    const protoEventSchedule = removeNullEvents(await this.ANNEngineInstance.computeProtoEventScheduleSegment(
+      this.terms, 
+      this.terms['statusDate'],
+      this.terms['maturityDate'],
+    ));
+
+    const endTimestamp = protoEventSchedule[Math.floor(protoEventSchedule.length / 2)].scheduleTime;
+
+    const { 0: state_computeNextState, 1: events_computeNextState } = await this.ANNEngineInstance.computeNextState(
+      this.terms, 
+      initialState, 
+      endTimestamp
+    );
+
+    let state_computeNextStateForProtoEvent = initialState;
+    let events_computeNextStateForProtoEvent = [];
+
+    for (let i = 0; i < 20; i ++) {
+      if (protoEventSchedule[i].scheduleTime > endTimestamp) { break; }
+
+      const { 0: nextState_computeNextStateForProtoEvent, 1: contractEvent } = await this.ANNEngineInstance.computeNextStateForProtoEvent(
+        this.terms, 
+        state_computeNextStateForProtoEvent, 
+        protoEventSchedule[i], 
+        protoEventSchedule[i].scheduleTime
+      );
+
+      contractEvent[4] = endTimestamp;
+      contractEvent.actualEventTime = endTimestamp;
+
+      state_computeNextStateForProtoEvent = nextState_computeNextStateForProtoEvent;
+      events_computeNextStateForProtoEvent.push(contractEvent);
+    }
+
+    assert.deepEqual(state_computeNextState, state_computeNextStateForProtoEvent);
+    assert.deepEqual(removeNullEvents(events_computeNextState), removeNullEvents(events_computeNextStateForProtoEvent));
   });
 
   it('should yield all events', async () => {
@@ -69,10 +115,5 @@ contract('ANNEngine', () => {
     protoEventSchedule = removeNullEvents(protoEventSchedule);
     
     assert.isTrue(protoEventSchedule.toString() === entireProtoEventSchedule.toString());
-  });
-
-  it('should yield the next next contract state and the contract events', async() => {
-    const initialState = await this.ANNEngineInstance.computeInitialState(this.terms, {});
-    await this.ANNEngineInstance.computeNextState(this.terms, initialState, this.terms['maturityDate']);
   });
 });
