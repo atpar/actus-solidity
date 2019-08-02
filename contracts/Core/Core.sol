de9317131ec02b7dc50118857010a9476a96dcad
jo-es <escherich.johannes+github@gmail.com>
2019-08-02 13:53:13 +0200

    Fix linting warnings

--- contracts/Core/Core.sol
+++ contracts/Core/Core.sol
@@ -38,25 +38,24 @@ contract Core is
 		pure
 		returns (uint256)
 	{
-		if (eventType == EventType.IED) { return 20; }
-		if (eventType == EventType.PR) { return 25; }
-		if (eventType == EventType.IP) { return 30; }
-		if (eventType == EventType.IPCI) { return 40; }
-		if (eventType == EventType.FP) { return 50; }
-		if (eventType == EventType.DV) { return 60; }
-		//if (eventType == EventType.PR) { return 70; }
-		if (eventType == EventType.MR) { return 80; }
-		if (eventType == EventType.RRY) { return 90; }
-		if (eventType == EventType.RR) { return 100; }
-		if (eventType == EventType.SC) { return 110; }
-		if (eventType == EventType.IPCB) { return 120; }
-		if (eventType == EventType.PRD) { return 130; }
-		if (eventType == EventType.TD) { return 140; }
-		if (eventType == EventType.STD) { return 150; }
-		if (eventType == EventType.MD) { return 160; }
-		if (eventType == EventType.SD) { return 900; }
-		if (eventType == EventType.AD) { return 950; }
-		if (eventType == EventType.Child) { return 10; }
+		if (eventType == EventType.IED) return 20;
+		if (eventType == EventType.PR) return 25;
+		if (eventType == EventType.IP) return 30;
+		if (eventType == EventType.IPCI) return 40;
+		if (eventType == EventType.FP) return 50;
+		if (eventType == EventType.DV) return 60;
+		if (eventType == EventType.MR) return 80;
+		if (eventType == EventType.RRY) return 90;
+		if (eventType == EventType.RR) return 100;
+		if (eventType == EventType.SC) return 110;
+		if (eventType == EventType.IPCB) return 120;
+		if (eventType == EventType.PRD) return 130;
+		if (eventType == EventType.TD) return 140;
+		if (eventType == EventType.STD) return 150;
+		if (eventType == EventType.MD) return 160;
+		if (eventType == EventType.SD) return 900;
+		if (eventType == EventType.AD) return 950;
+		if (eventType == EventType.Child) return 10;
 		return 0;
 	}
 }
