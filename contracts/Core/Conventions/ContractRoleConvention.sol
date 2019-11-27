e8836ce8664fe6ccf870fcaae3553814d76b8929
jo-es <escherich.johannes+github@gmail.com>
2019-11-27 16:02:16 +0100

    Add ContractRole convention for BUY, SEL, RFL, PFL

--- contracts/Core/Conventions/ContractRoleConvention.sol
+++ contracts/Core/Conventions/ContractRoleConvention.sol
@@ -12,6 +12,13 @@ contract ContractRoleConvention is Definitions {
 	{
 		if (contractRole == ContractRole.RPA) return 1;
 		if (contractRole == ContractRole.RPL) return -1;
+    
+    if (contractRole == ContractRole.BUY) return 1;
+    if (contractRole == ContractRole.SEL) return -1;
+    
+    if (contractRole == ContractRole.RFL) return 1;
+    if (contractRole == ContractRole.PFL) return -1;
+    
 		revert("ContractRoleConvention.roleSign: ATTRIBUTE_NOT_FOUND");
 	}
 }
